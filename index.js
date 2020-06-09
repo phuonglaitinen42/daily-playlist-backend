3907169ca8955052f16ac6e050d821a772eaa81e
otsolap <59653348+otsolap@users.noreply.github.com>
2020-06-09 10:41:52 +0300

    [routes-spotify-merged] ready for testing with Pham Vy. everything running on 3001. TRIAL RUN!

--- index.js
+++ index.js
@@ -2,26 +2,30 @@
 // Just like we did with Login-app in Node-course.
 var express = require("express"); // Express web server framework
 const app = express();
-const mongoose = require('mongoose');
 const usernameRouter = require('./api/usernames/username.router');
 const PORT = process.env.PORT || 3001
 const FRONTEND_ORIGIN = "http://localhost:3000";
 
-var server = require("server"); // require server.js
+const server = require("./server"); // require server.js
 app.use(server); // mount server.js as a sub-app.
-var call = require("call"); // require call,js
-app.use(call); // mount call.js as sub-app.
 
+// .env required
+require('dotenv').config();
+
+  const mongoose = require('mongoose');
+var URL = process.env.MONGOLAB_URI;
 // Connection to our Database
 mongoose.connect(URL,
-  // will make code into json
-  { useNewUrlParser: true }, () =>
-  console.log('Connected to DB!')
+// will make code into json
+{ useNewUrlParser: true }, () =>
+console.log('Connected to DB!')
 );
+
 // making sure MongoDB is active.
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'DB connection error: '));
 db.on('open', console.log.bind(console, 'Connected to MongoDB!'));
+
 
 // Chrome does ajax calls.
 app.use((req, res, next) => {
@@ -39,8 +43,6 @@ app.use('/usernames', usernameRouter);
 app.get('/api', (req, res, next) => {
   res.send('API status: Running')
 });
-
-
 
 app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
