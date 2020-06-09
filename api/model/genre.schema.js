defdfc51449458a5240be5a270b201b23918b113
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-06-09 14:15:07 +0300

    testing how to fetch sepcific result id

--- api/model/genre.schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const GenreSchema = mongoose.Schema({
-  genre: {
-    type: String,
-    required: true,
-  },
-});
-
-module.exports = mongoose.model("Genre", GenreSchema);
